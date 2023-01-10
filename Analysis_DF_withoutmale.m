9036004d4b3128429d1fd697a16c0f9f5325c0fe
Zhenpeng Ge <sklec_gezhenpeng@126.com>
2023-01-10 21:41:40 +0800

    Update Analysis_DF_withoutmale.m

--- Analysis_DF_withoutmale.m
+++ Analysis_DF_withoutmale.m
@@ -29,10 +29,10 @@ Mean_2005 = zeros(MC,length(Scale)) ;
 Mean_2006 = zeros(MC,length(Scale)) ;
 Mean_2007 = zeros(MC,length(Scale)) ;
 for i = 1:MC
-    [VMR_2003(i,:),Mean_2003(i,:),Var_2003(i,:)] = Vector_TL_DF_square_mark(Data2003,Scale,1000) ;
-    [VMR_2004(i,:),Mean_2004(i,:),Var_2004(i,:)] = Vector_TL_DF_square_mark(Data2004,Scale,1000) ;
-    [VMR_2005(i,:),Mean_2005(i,:),Var_2005(i,:)] = Vector_TL_DF_square_mark(Data2005,Scale,1000) ;
-    [VMR_2006(i,:),Mean_2006(i,:),Var_2006(i,:)] = Vector_TL_DF_square_mark(Data2006,Scale,1000) ;
-    [VMR_2007(i,:),Mean_2007(i,:),Var_2007(i,:)] = Vector_TL_DF_square_mark(Data2007,Scale,1000) ;
+    [VMR_2003(i,:),Mean_2003(i,:),Var_2003(i,:)] = Vector_DF_square_mark(Data2003,Scale,1000) ;
+    [VMR_2004(i,:),Mean_2004(i,:),Var_2004(i,:)] = Vector_DF_square_mark(Data2004,Scale,1000) ;
+    [VMR_2005(i,:),Mean_2005(i,:),Var_2005(i,:)] = Vector_DF_square_mark(Data2005,Scale,1000) ;
+    [VMR_2006(i,:),Mean_2006(i,:),Var_2006(i,:)] = Vector_DF_square_mark(Data2006,Scale,1000) ;
+    [VMR_2007(i,:),Mean_2007(i,:),Var_2007(i,:)] = Vector_DF_square_mark(Data2007,Scale,1000) ;
 end
 save('Withoutmale_TL_DF.mat') ;
